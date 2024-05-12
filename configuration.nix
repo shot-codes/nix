d8cd3bbf539e86e7a05052d8cf7468e0d9c5232f
Nicolai Verbaarschot <nicolai.verbaarschot@gmail.com>
2024-05-12 16:33:49 +0200

    Added greetd.

--- configuration.nix
+++ configuration.nix
@@ -30,7 +30,28 @@ hardware.pulseaudio.enable = true;
 environment.systemPackages = with pkgs; [
 	git
 	vim
+	lemurs
 ];
+
+services.greetd = {
+	enable = true;
+	restart = false;
+	settings.default_session = {
+		command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-session --sessions ${inputs.hyprland.packages.${pkgs.system}.hyprland}/share/wayland-sessions";
+	#	command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd ${pkgs.hyprland}/bin/Hyprland";
+		user = "greeter";
+	};
+};
+
+#systemd.services.greetd.serviceConfig = {
+#	Type = "idle";
+#	StandardInput = "tty";
+#	StandardOutput = "tty";
+#	StandardError = "tty";
+#	TTYReset = true;
+#	TTYVHangup = true;
+#	TTYVTDisallocate = true;
+#};
 
 users.users.shot = {
 	isNormalUser = true;
