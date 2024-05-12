128a6661892cfeb794038bb7b39c7ef7dd822932
Nicolai Verbaarschot <nicolai.verbaarschot@gmail.com>
2024-05-13 00:22:15 +0200

    slight additions.

--- configuration.nix
+++ configuration.nix
@@ -11,138 +11,85 @@
 	./disk-config.nix
     ];
 
-nix.settings.experimental-features = [ "nix-command" "flakes" ];
-nix.settings.substituters = ["https://hyprland.cachix.org"];
-nix.settings.trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.substituters = ["https://hyprland.cachix.org"];
+  nix.settings.trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-time.timeZone = "Europe/Copenhagen";
-i18n.defaultLocale = "en_US.UTF-8";
+  time.timeZone = "Europe/Copenhagen";
+  i18n.defaultLocale = "en_US.UTF-8";
+  
+  networking.hostName = "GLaDOS";
+  networking.networkmanager.enable = true;
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+  
+  environment.systemPackages = with pkgs; [
+  	git
+  	vim
+    neovide
+  ];
 
-networking.hostName = "GLaDOS";
-networking.networkmanager.enable = true;
-sound.enable = true;
-hardware.pulseaudio.enable = true;
-
-environment.systemPackages = with pkgs; [
-	git
-	vim
-];
-
-services.greetd = {
-	enable = true;
-	restart = false;
-	settings.default_session = {
-		command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-session --sessions ${inputs.hyprland.packages.${pkgs.system}.hyprland}/share/wayland-sessions";
-	#	command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd ${pkgs.hyprland}/bin/Hyprland";
-		user = "greeter";
-	};
-};
-
-#systemd.services.greetd.serviceConfig = {
-#	Type = "idle";
-#	StandardInput = "tty";
-#	StandardOutput = "tty";
-#	StandardError = "tty";
-#	TTYReset = true;
-#	TTYVHangup = true;
-#	TTYVTDisallocate = true;
-#};
-
-users.users.shot = {
-	isNormalUser = true;
-	extraGroups = [ "networkmanager" "wheel" ];
-};
-
-programs.hyprland = { 
-	enable = true;
-	package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-};
-
-programs.zsh.enable = true;
-users.defaultUserShell = pkgs.zsh;
-
-hardware.tuxedo-rs = {
-	enable = true;
-	tailor-gui.enable = true;
-};
-
-
-  # networking.hostName = "nixos"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-
-  # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;  
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.alice = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     firefox
-  #     tree
-  #   ];
-  # };
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  fonts = {
+    enableDefaultPackages = true;
+    packages = with pkgs; [ 
+      iosevka
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      source-code-pro
+      (nerdfonts.override { fonts = [ "Iosevka" ]; })
+    ];
+  
+    fontconfig = {
+      defaultFonts = {
+        serif = [  "IosevkaNerdFont" ];
+        sansSerif = [ "IosevkaNerdFont" ];
+        monospace = [ "IosevkaNerdFont" ];
+      };
+    };
+  };
+  
+  services.greetd = {
+  	enable = true;
+  	restart = false;
+  	settings.default_session = {
+  		command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-session --sessions ${inputs.hyprland.packages.${pkgs.system}.hyprland}/share/wayland-sessions";
+  	#	command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd ${pkgs.hyprland}/bin/Hyprland";
+  		user = "greeter";
+  	};
+  };
+  
+  systemd.services.greetd.serviceConfig = {
+  	Type = "idle";
+  	StandardInput = "tty";
+  	StandardOutput = "tty";
+  	StandardError = "tty";
+  	TTYReset = true;
+  	TTYVHangup = true;
+  	TTYVTDisallocate = true;
+  };
+  
+  users.users.shot = {
+  	isNormalUser = true;
+  	extraGroups = [ "networkmanager" "wheel" ];
+  };
+  
+  programs.hyprland = { 
+  	enable = true;
+  	package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+  };
+  
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
+  
+  hardware.tuxedo-rs = {
+  	enable = true;
+  	tailor-gui.enable = true;
+  };
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
--- home.nix
+++ home.nix
@@ -18,13 +18,55 @@
 
 	programs.kitty = {
 		enable = true;
+    extraConfig = ''
+		window_padding_width 4
+
+		foreground              #cecece
+		background              #000000
+		selection_foreground    #cecece
+		selection_background    #293334
+		cursor                  #cd974b
+		cursor_text_color       #000000
+		url_color               #cd974b
+		#: black
+		color0                  #000000
+		color8                  #777777
+		#: red
+		color1                  #e25d56
+		color9                  #f36868
+		#: green
+		color2                  #73ca50
+		color10                 #88db3f
+		#: yellow
+		color3                  #e9bf57
+		color11                 #f0bf7a
+		#: blue
+		color4                  #4a88e4
+		color12                 #6f8fdb
+		#: magenta
+		color5                  #915caf
+		color13                 #e987e9
+		#: cyan
+		color6                  #23acdd
+		color14                 #4ac9e2
+		#: white
+		color7                  #cecece
+		color15                 #ffffff
+		
+		active_border_color             #777777
+		inactive_border_color           #323738
+		bell_border_color               #e97e57
+		active_tab_foreground           #cecece
+		active_tab_background           #0e1415
+		inactive_tab_foreground         #b8b8b8
+		inactive_tab_background         #323738 
+    '';
 	};
 
 	programs.firefox.enable = true;
 	
 	programs.neovim = {
 		enable = true;
-		defaultEditor = true;
 		extraPackages = with pkgs; [
 			gcc
 		];
@@ -51,7 +93,26 @@
 				"clickfinger_behavior" = true;
 			};
 		};
+		"decoration" = {
+			"rounding" = 5;
+		};
+		"misc" = {
+				"disable_hyprland_logo" = true;
+			};
 	};
+	wayland.windowManager.hyprland.extraConfig = ''
+		animations {
+		    enabled = yes
+		    bezier = myBezier, 0.05, 0.9, 0.1, 1.0
+		    bezier = myBezier2, 0.0, 0.1, 0.0, 1.0
+		    animation = windows, 1, 3, myBezier
+		    animation = windowsOut, 1, 3, default, popin 80%
+		    animation = border, 1, 20, default
+		    animation = borderangle, 1, 8, default
+		    animation = fade, 1, 7, default
+			animation = workspaces, 0, 2, myBezier2
+		}
+	'';
 
 	home.stateVersion = "23.11";
 
