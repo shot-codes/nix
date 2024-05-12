3d4ccb31a7886c8c595fa77cddc71cd6fbe8beb7
Nicolai Verbaarschot <nicolai.verbaarschot@gmail.com>
2024-05-12 14:22:34 +0200

    Hyprland now using cahix to get latest commit.

--- flake.nix
+++ flake.nix
@@ -23,6 +23,7 @@
 				}
 				home-manager.nixosModules.home-manager
 				{
+					home-manager.extraSpecialArgs = { inherit inputs; };
 					home-manager.useGlobalPkgs = true;
 					home-manager.useUserPackages = true;
 					home-manager.users.shot = import ./home.nix;
--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
 	home.username = "shot";
@@ -20,8 +20,12 @@
 		enable = true;
 	};
 
+	programs.firefox.enable = true;
+
 	wayland.windowManager.hyprland.enable = true;
+	wayland.windowManager.hyprland.package = inputs.hyprland.packages.${pkgs.system}.hyprland;
 	wayland.windowManager.hyprland.settings = {
+		"monitor" = "eDP-1,2560x1600@240.00301,0x0,1";
 		"$mod" = "SUPER";
 		bind = [
 			"$mod, Return, exec, kitty"
