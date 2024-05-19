4cf01db30fa0db0c8aae6b91a4130dcd6a0fe80c
Nicolai Verbaarschot <nicolai.verbaarschot@gmail.com>
2024-05-19 17:00:10 +0200

    Removed darkman; using dconf directly.

--- home/gtk.nix
+++ home/gtk.nix
@@ -13,33 +13,4 @@
     color-scheme = "prefer-dark";
     gtk-theme = "Materia-dark-compact";
   };
-
-  services.darkman = {
-    enable = true;
-    settings = {
-      lat = 55.68;
-      lng = 12.59;
-      usegeoclue = true;
-    };
-    darkModeScripts = {
-      gtk-cheme = ''
-        ${pkgs.dconf}/bin/dconf write \
-            /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
-      '';
-      gtk-theme = ''
-        ${pkgs.dconf}/bin/dconf write \
-            /org/gnome/desktop/interface/gtk-theme "'Materia-dark-compact'"
-      '';
-    };
-    lightModeScripts = {
-      gtk-scheme = ''
-        ${pkgs.dconf}/bin/dconf write \
-            /org/gnome/desktop/interface/color-scheme "'prefer-light'"
-      '';
-      gtk-theme = ''
-        ${pkgs.dconf}/bin/dconf write \
-            /org/gnome/desktop/interface/gtk-theme "'Materia-light-compact'"
-      '';
-    };
-  };
 }
--- home/hypr/scripts/toggle_theme.sh
+++ home/hypr/scripts/toggle_theme.sh
@@ -4,12 +4,14 @@ if [ "$PREFERS" == "prefer-light" ]; then
 	# hyprctl keyword general:col.active_border "rgba(ffa100ff) rgba(ff2a00ff) 45deg"
 	echo "font_family Iosevka NF" >/home/shot/.config/kitty/font.conf
 	kitten themes --reload-in=all --cache-age=-1 --config-file-name /home/shot/.config/kitty/theme.conf Alabaster Dark Custom
-	darkman set dark
+	dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
+	dconf write /org/gnome/desktop/interface/gtk-theme "'Materia-dark-compact'"
 	for f in /run/user/1000/nvim.*; do nvim --server "$f" --headless --remote-expr 'execute("set background=dark")' &>/dev/null; done
 else
 	# hyprctl keyword general:col.active_border "rgba(33ccffff) rgba(00ff99ff) 45deg"
 	echo "font_family Iosevka NF Bold" >/home/shot/.config/kitty/font.conf
 	kitten themes --reload-in=all --cache-age=-1 --config-file-name /home/shot/.config/kitty/theme.conf Alabaster Custom
-	darkman set light
+	dconf write /org/gnome/desktop/interface/color-scheme "'prefer-light'"
+	dconf write /org/gnome/desktop/interface/gtk-theme "'Materia-light-compact'"
 	for f in /run/user/1000/nvim.*; do nvim --server "$f" --headless --remote-expr 'execute("set background=light")' &>/dev/null; done
 fi
