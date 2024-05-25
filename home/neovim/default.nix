7c834f0d8a35215a26cfc766feb8d6b0ebaccf88
Nicolai Verbaarschot <nicolai.verbaarschot@gmail.com>
2024-05-25 19:20:54 +0200

    nixvim with custom alabaster.

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -8,5 +8,26 @@
   ];
   programs.nixvim = {
     enable = true;
+    defaultEditor = true;
+    extraPlugins = [
+      (pkgs.vimUtils.buildVimPlugin {
+        name = "alabaster";
+        src = pkgs.fetchFromGitHub {
+          owner = "shot-codes";
+          repo = "alabaster.nvim";
+          rev = "2916f15bfd0f77c2b320f0badd338ea2d00add93";
+          hash = "sha256-1n9APeQK8lFFv5i+6n7ZkBIxHb/M8goXTftDqWVV/i4=";
+        };
+      })
+    ];
+    colorscheme = "alabaster";
+
+    plugins = {
+      telescope.enable = true;
+      harpoon = {
+        enable = true;
+        enableTelescope = true;
+      };
+    };
   };
 }
