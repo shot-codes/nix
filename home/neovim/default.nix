c1777ea48f9b76844fe2f2862041704ad95beb8b
Nicolai Verbaarschot <nicolai.verbaarschot@gmail.com>
2024-05-27 10:17:04 +0200

    nixvim config stuff.

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -8,7 +8,15 @@
   ];
   programs.nixvim = {
     enable = true;
+    viAlias = true;
     defaultEditor = true;
+    colorscheme = "alabaster";
+    clipboard.register = "unnamedplus";
+
+    globals = {
+      mapleader = " ";
+    };
+
     extraPlugins = [
       (pkgs.vimUtils.buildVimPlugin {
         name = "alabaster";
@@ -20,13 +28,35 @@
         };
       })
     ];
-    colorscheme = "alabaster";
+
+    keymaps = [
+      {
+        mode = "i";
+        action = "<Esc>";
+        key = "jk";
+        options = {
+          noremap = true;
+          silent = true;
+        };
+      }
+    ];
 
     plugins = {
       telescope.enable = true;
       harpoon = {
         enable = true;
         enableTelescope = true;
+        keymapsSilent = true;
+        keymaps = {
+          addFile = "<leader>ha";
+          toggleQuickMenu = "<C-e>";
+          navFile = {
+            "1" = "<leader>hj";
+            "2" = "<leader>hk";
+            "3" = "<leader>hl";
+            "4" = "<leader>hm";
+          };
+        };
       };
     };
   };
