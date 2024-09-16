bcac1480b7711c2dfcc8f9233c09ac1dc0648a94
Nicolai Verbaarschot <nicolai.verbaarschot@gmail.com>
2024-09-16 20:26:00 +0200

    added pipewire

--- system/default.nix
+++ system/default.nix
@@ -46,7 +46,14 @@
       "192.168.0.102" = ["puzzlerhome"];
     };
   };
-  hardware.pulseaudio.enable = true;
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
 
   environment.systemPackages = with pkgs; [
     git
